4a6f22860e331c29a5eb487a178eb6a670a96694
okafor echezona <okaforechezona1992@gmail.com>
2019-10-25 12:19:21 +0100

    adding new startup config

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "My go programming App")
+	fmt.Fprintf(w, "My go programming App with mux")
 }
 
 func setupRoutes() {
@@ -14,7 +16,8 @@ func setupRoutes() {
 }
 
 func main() {
+	r := mux.NewRouter()
 	fmt.Println("Go Web App Started on Port 3000")
 	setupRoutes()
-	http.ListenAndServe(":4000", nil)
+	http.ListenAndServe(":5000", r)
 }
