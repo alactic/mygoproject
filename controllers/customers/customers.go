77d54e1413af86c137a5dea22c29586bd688f44b
okafor echezona <okaforechezona1992@gmail.com>
2019-10-25 14:30:15 +0100

    updating the config

--- controllers/customers/customers.go
+++ controllers/customers/customers.go
@@ -19,6 +19,14 @@ type Customer = customers.Customer
 
 var bucket *gocb.Bucket = connection.Connection()
 
+//router.HandleFunc("/customers", GetCustomersEndpoint).Methods("GET")
+// //router.HandleFunc("/customers", GetCustomersEndpoint).Methods("GET")
+func GetIndexEndpoint(response http.ResponseWriter, request *http.Request) {
+		response.Write([]byte(`{"message": "my go demo project is working"}`))
+}
+
+
+
 //router.HandleFunc("/customer", CreateCustomerEndpoint).Methods("POST")
 func CreateCustomerEndpoint(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("Content-Type", "application/json")
@@ -155,6 +163,30 @@ func ReadFile(w http.ResponseWriter, r *http.Request) {
 	// 	fmt.Println(err)
 	// }
 	// reader, _ := os.Open("uploads/upload-189240647.png")
+	// var Buf bytes.Buffer
+	// // in your case file would be fileupload
+	// file, header, err := r.FormFile("/uploads/orange.jpg")
+	// if err != nil {
+	// 	fmt.Println("working with file error :: ", err)
+	// 	panic(err)
+	// }
+	// defer file.Close()
+	// name := strings.Split(header.Filename, ".")
+	// fmt.Printf("File name %s\n", name[0])
+	// // Copy the file data to my buffer
+	// io.Copy(&Buf, file)
+	// // do something with the contents...
+	// // I normally have a struct defined and unmarshal into a struct, but this will
+	// // work as an example
+	// contents := Buf.String()
+	// fmt.Println(contents)
+	// I reset the buffer in case I want to use it again
+	// reduces memory allocations in more intense projects
+	// Buf.Reset()
+	// do something else
+	// etc write header
+	return
+}
 	// var Buf bytes.Buffer
 	// // in your case file would be fileupload
 	// file, header, err := r.FormFile("/uploads/orange.jpg")
--- routes/customers/customers.go
+++ routes/customers/customers.go
@@ -7,9 +7,11 @@ import (
 )
 
 func Customers(router *mux.Router) {
+	router.HandleFunc("/", customers.GetIndexEndpoint).Methods("GET")
 	router.HandleFunc("/customer", authentication.AuthMiddleware(customers.CreateCustomerEndpoint)).Methods("POST")
 	router.HandleFunc("/customer/{id}", authentication.AuthMiddleware(customers.GetCustomerEndpoint)).Methods("GET")
 	router.HandleFunc("/customers", customers.GetCustomersEndpoint).Methods("GET")
 	router.HandleFunc("/uploads", authentication.AuthMiddleware(customers.UploadFile)).Methods("POST")
 	router.HandleFunc("/readFiles", customers.ReadFile).Methods("GET")
+}
 }
