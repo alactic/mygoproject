aedfa9bd1008f58b84dd107b202276df8d9505fb
alactic <bluecard1992@yahoo.com>
2019-11-02 18:38:02 +0100

    add new feature

--- main.go
+++ main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/alactic/mygoproject/utils/connection"
 	"github.com/alactic/mygoproject/utils/router"
 	"gopkg.in/couchbase/gocb.v1"
+	"os"
 )
 
 var bucket *gocb.Bucket
 
 func main() {
 	fmt.Println("bucket name1 ELVIS ")
+	fmt.Println("host name 1:: ", os.Getenv("COUCHBASEHOST"))
+	fmt.Println("Starting go1 lang application ...")
+
+	fmt.Println("host name :: ", os.Getenv("COUCHBASEHOST"))
+	fmt.Println("bucket name :: ", bucket.Name())
 
 	bucket = connection.Connection()
 	router.Router()
