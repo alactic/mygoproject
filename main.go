562b2ebfac90bb0bf8974803a76de97acca1fccc
alactic <bluecard1992@yahoo.com>
2019-11-19 14:18:37 +0100

    add new feature

--- main.go
+++ main.go
@@ -5,15 +5,12 @@ import (
 	"github.com/alactic/mygoproject/utils/connection"
 	"github.com/alactic/mygoproject/utils/router"
 	"gopkg.in/couchbase/gocb.v1"
-	"os"
 )
 
 var bucket *gocb.Bucket
 
 func main() {
 	fmt.Println("starting application")
-	os.Setenv("SO_TIMEOUT", "8000")
-	os.Setenv("CONNECTION_TIMEOUT", "12000")
 	bucket = connection.Connection()
 	router.Router()
 }
